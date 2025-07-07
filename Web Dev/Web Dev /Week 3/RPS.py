c9730b4c7ac579f520d6f162a701f4cb8d1902e6
Kristopher Ross <Kristopher.Ross@hgs.hiddengeniusproject.org>
2025-07-07 15:39:18 -0700

    updated profollio

---
+++ Web Dev/Web Dev /Week 3/RPS.py
@@ -0,0 +1,53 @@
+# build a RPS game 
+# 2-players 
+# each player picks bewtween rock paper or scissors
+# each players choice is compared:
+# Rock < Scissors 
+
+def RPS():
+    player1 = input("player 1, please enter your name: ")
+    player2 = input("player 1, please enter your name: ")
+
+    p1_choice = input(f"{player1}, choose bewtween Rock, Paper, Scissors ").lower().strip()
+
+    while not IsValidmove(p1_choice):
+        print("InvalidMove please try again")
+        p1_choice = input(f"{player1}, choose bewtween Rock, Paper, Scissors ").lower().strip()
+
+
+    p2_choice = input(f"{player2}, choose bewtween Rock, Paper").lower().strip()
+
+    while not IsValidmove(p2_choice):
+        print("InvalidMove please try again")
+        p2_choice = input(f"{player2}, choose bewtween Rock, Paper, Scissors ").lower().strip()
+
+
+    if p1_choice == "rock" and p2_choice =="paper":
+        print(f"Rock beats Sciossrs, {player1} wins")
+    elif p1_choice == "scissors" and p2_choice == "paper":
+        print(f"paper beats rock ,  {player1} wins!")
+    elif p1_choice == "paper" and p2_choice == "rock":
+        print(f"paper beats rock ,  {player1} wins!")
+     
+    elif p2_choice == "rock" and p1_choice == "paper":
+        print(f"paper beats rock ,  {player1} wins!")
+
+    elif p2_choice == "scissors" and p1_choice == "paper":
+        print(f"paper beats rock ,  {player1} wins!")
+
+    elif p2_choice == "paper" and p1_choice == "rock":
+        print(f"paper beats rock ,  {player2} wins!")
+
+
+
+
+
+def IsValidmove(playermove):
+    validmoves = ["rock", "paper", "scissors"]
+
+    if playermove in validmoves: 
+        return True
+    else:
+        return False 
+
+RPS()
---
+++ python/Week 4 /dataStructures.py
@@ -0,0 +1,11 @@
+favsnacks =["Starburst", "Gummies","Doritos","Welches","Dinamite chips"]
+favsnacks.append("Twix")
+favsnacks.sort()
+for snack in favsnacks:
+    print(snack) 
+
+
+    myColleges = ("Moore house", "Howard", "USC", "Stanford", "CCC")
+    
+    for college in myColleges: 
+        print(college)
