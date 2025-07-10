5a0a3ec4d26008b2069608ae949ef4e5c82c55fe
Kristopher Ross <Kristopher.Ross@hgs.hiddengeniusproject.org>
2025-07-10 14:29:26 -0700

    data scnarios

---
+++ python/Week 4 /TravelBag.md
@@ -0,0 +1,10 @@
+1. What is one difference between a list and a tuple?
+
+
+2. When is it better to use a list over a tuple?
+
+
+3. How do you remove an item from a list?
+
+
+4. Name at least 2 mutation functions you can use on li
---
+++ python/Week 4 /datascenarios.py
@@ -0,0 +1,19 @@
+print("Scenario #1: A restarant menu with prices for each item") 
+print("Best structure: Dictonary; best to pair food with price in key/value") 
+menu = {
+    "French Toast": 12,
+    "Grand Slam": 12, 
+    "T-Bone Steak": 18, 
+    "Avacado Toast": 15, 
+}
+
+for Key, item in menu.items():
+    
+ 
+
+
+
+
+
+
+
